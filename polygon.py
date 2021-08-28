ff45858ab8aaf01a1716bdb0569ecd5e3a41c8e1
Mosaab Gamal Kamel Abdelaleem <85448475+mosaabg@users.noreply.github.com>
2021-08-28 21:51:11 +0200

    Update polygon.py

--- polygon.py
+++ polygon.py
@@ -1,17 +1,23 @@
 import math
 import turtle
+from colorama import *
 
-print ("Hello, this is an application that does some functions related to regular Polygons.")
 
-print ("Choose from these options:")
-print ("[1] Calculate the area of a regular polygon")
-print ("[2] Calculate the perimeter of a regular polygon")
-print ("[3] Calculate the measurement of this polygon's single angle")
-print ("[4] Draw a regular polygon")
-Choice = int(input("Choose number of program to execute: "))
+print (Style.BRIGHT + Fore.BLUE + "Hello, this is an application that does some functions related to regular Polygons.")
 
-Sides_Number = int(input("Input number of sides: "))
-Side_Length = float(input("Input the lenght of the side: "))
+print (Fore.YELLOW +"Choose from these options:")
+print (Fore.YELLOW +"[1] Calculate the area of a regular polygon")
+print (Fore.YELLOW +"[2] Calculate the perimeter of a regular polygon")
+print (Fore.YELLOW +"[3] Calculate the measurement of this polygon's single angle")
+print (Fore.YELLOW +"[4] Draw a regular polygon")
+Choice = int(input(Fore.GREEN +"Choose number of program to execute: "))
+if Choice < 1 or Choice > 4:
+    print (Fore.RED +"Wrong program choice...")
+    print (Style.RESET_ALL)
+    exit()
+
+Sides_Number = int(input(Fore.GREEN +"Input number of sides: "))
+Side_Length = float(input(Fore.GREEN +"Input the lenght of the side: "))
 
 Polygon_Perimeter = Sides_Number * Side_Length
 Polygon_Apothem = Side_Length / (2 * math.tan(math.pi / Sides_Number))
@@ -19,13 +25,13 @@ Polygon_Area = Polygon_Perimeter * Polygon_Apothem / 2
 Angle_Measure = 2 * 180 / Sides_Number
 
 def Area_Func():
-    print("Polygon Area = " + str(round(Polygon_Area, 3)))
+    print(Fore.BLUE + "Polygon Area = " + str(round(Polygon_Area, 3)))
 
 def Perimeter_Func():
-    print("Polygon perimeter = " + str(round(Polygon_Perimeter, 3)))
+    print(Fore.BLUE + "Polygon perimeter = " + str(round(Polygon_Perimeter, 3)))
 
 def Angle_Func():
-    print("The measurement of every single angle = " + str(round(Angle_Measure, 3)))
+    print(Fore.BLUE + "The measurement of every single angle = " + str(round(Angle_Measure, 3)))
 
 def Draw_Func():
     for _ in range(Sides_Number):
@@ -46,4 +52,4 @@ elif Choice == 3:
 elif Choice == 4:
     Draw_Func()
 
-else: print ("Wrong choice...")
+print(Style.RESET_ALL)
